47eed762ad0c58f5b82a5eece9c2f70e2d0d6ebc
Zachary McDonald <zac.mcdonald1013@gmail.com>
2024-02-08 14:19:14 -0600

    updates to ensembl (untested)

--- fetchdata_ensembl.cpp
+++ fetchdata_ensembl.cpp
@@ -1,4 +1,5 @@
 #include "fetchdata_ensembl.h"
+#include "fetchdata_dbsnp.h"
 
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
@@ -9,60 +10,32 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
 
 void create_ensembl_url(string& gene_name, string& url)
 {
-    string urlA = "https://rest.ensembl.org/sequence/id/";
+    string urlA = "https://rest.ensembl.org/lookup/symbol/homo_sapiens/";
     string urlB = gene_name;
-    string urlC = "?";
+    string urlC = "?content-type=application/json";
 
     url = urlA + urlB + urlC;
 }
 
-void get_gene_sequence(string& gene_name, string& gene_sequence)
+string get_gene_length(string& gene_name)
 {
-    //curl 'https://rest.ensembl.org/sequence/id/ENSG00000157764?' -H 'Content-type:text/plain'
-
-    string url; 
+    string url;
     create_ensembl_url(gene_name, url);
 
-    CURL* curl;
+    Json::Value obj;
+    get_json_from_url(url, obj);
 
-    string readBuffer;
-
-    int attempts = 0;
-    while (readBuffer.size() < 1)
-    {
-        curl = curl_easy_init();
-
-        if(curl) 
-        {
-            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-            curl_easy_perform(curl);
-            curl_easy_cleanup(curl);
-        }
-
-        cout << readBuffer << endl;
-        
-        if (attempts > 10)
-        {
-            cerr << "***ERROR*** Failed to retreive data from " << url << endl;
-            break;
-        }
-        attempts++;
-    }
-
-    if (DEBUG)
-    {
-        ofstream output;
-        output.open("debug/DEBUG_json_obj_ensembl.txt");
-        output << readBuffer << endl;
-        output.close();
-    }
+    return obj["Transcript"][0]["Length"].asString();
 }
 
-int get_gene_length(string& gene_name)
+void add_gene_length_to_results(matrix3d& r)
 {
-    string gene_sequence;
-    get_gene_sequence(gene_name, gene_sequence);
-    return gene_sequence.size();
+    r[0].push_back(string_to_vec("Gene Length"));
+
+    int name_loc = 4;
+
+    for (int i = 1; i < r.size(); i++)
+    {
+        r[i].push_back( string_to_vec( get_gene_length(r[i][name_loc][0]) ) );
+    }
 }
